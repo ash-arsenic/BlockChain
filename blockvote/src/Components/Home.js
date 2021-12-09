0bd155dc9045e94318741dbd159ef87ca12b2d83
Ash Arsenic <ashchauhan498@gmail.com>
2021-12-09 20:08:35 +0530

    Retrieving data

--- blockvote/src/Components/Home.js
+++ blockvote/src/Components/Home.js
@@ -1,43 +1,52 @@
-import {Tab} from "bootstrap";
-import React from 'react';
-import {Table,Container,Button} from "react-bootstrap";
-
-const Home = props => {
-    const promptList=[
-        "who would win in smash bros?",
-        "who is the better actor?"
-];
-    return (
-        <Container>
-                <Table style={{ margin: "5vh"}} stripped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>List of Polls</th>
-                            <th>Go to Poll</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            promptList.map((el,index) =>{
-                                return(
-                                    <tr key={index}>
-                                        <td>{index+1}</td>
-                                        <td>{el}</td>
-                                        <td>
-                                            {" "}
-                                            <Button onClick={()=>props.changeCandidates(el)}>Go to Poll</Button>
-                                        </td>
-                                    </tr>
-                                );
-                            }
-                            )}
-                    </tbody>
-                </Table>
-        </Container>
-    );
+import { Tab } from "bootstrap";
+import React, { useEffect } from "react";
+import { Table, Container, Button } from "react-bootstrap";
+import { useState } from "react";
+const Home = (props) => {
+  const [promptList, setPromptList] = useState([]);
+  // https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/220px-Cristiano_Ronaldo_2018.jpg
+  //   https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg
+  const loadData = async () => {
+    try {
+      const prompts = await window.contract.getAllPrompts();
+      console.log(prompts);
+      setPromptList(prompts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    loadData();
+  }, []);
+  return (
+    <Container>
+      <Table style={{ margin: "5vh" }} stripped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>List of Polls</th>
+            <th>Go to Poll</th>
+          </tr>
+        </thead>
+        <tbody>
+          {promptList.map((el, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{el}</td>
+                <td>
+                  {" "}
+                  <Button onClick={() => props.changeCandidates(el)}>
+                    Go to Poll
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
-
-
 
 export default Home;
--- blockvote/src/Components/NewPoll.js
+++ blockvote/src/Components/NewPoll.js
@@ -9,71 +9,80 @@ const NewPoll = (props) => {
   const candidateName2URL = useRef();
 
   const promptRef = useRef();
-  
 
-  const sendToBloackChain=async() => {
-    await window.contract.addUrl({
-      name: candidateName1.current.value,
-      url:candidateName1URL.current.value,
-    });
-    await window.contract.addUrl({
-      name: candidateName2.current.value,
-      url:candidateName2URL.current.value,
-    });
+  const sendToBloackChain = async () => {
+    try {
+      await window.contract.addUrl({
+        name: candidateName1.current.value,
+        url: candidateName1URL.current.value,
+      });
+      await window.contract.addUrl({
+        name: candidateName2.current.value,
+        url: candidateName2URL.current.value,
+      });
 
-    await window.contract.addCandidatePair({
-      prompt: promptRef.current.value,
-      name1: candidateName1.current.value,
-      name2: candidateName2.current.value,
-    });
+      await window.contract.addCandidatePair({
+        prompt: promptRef.current.value,
+        name1: candidateName1.current.value,
+        name2: candidateName2.current.value,
+      });
 
-
-
-    await window.contract.addToPromptArray({ prompt:promptRef.current.value});
+      await window.contract.addToPromptArray({
+        prompt: promptRef.current.value,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
-  
+
   return (
     <Container style={{ marginTop: "10px" }}>
       <Form>
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>Candidiate 1 Name</Form.Label>
           <Form.Control
             ref={candidateName1}
-            placeholder='Enter Candidate Name'
+            placeholder="Enter Candidate Name"
+            value="Cristiano Ronaldo"
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>Candidate 1 Image URL</Form.Label>
           <Form.Control
             ref={candidateName1URL}
-            placeholder='enter Image URL'
+            placeholder="enter Image URL"
+            value="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/220px-Cristiano_Ronaldo_2018.jpg"
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>Candidiate 2 Name</Form.Label>
           <Form.Control
             ref={candidateName2}
-            placeholder='Enter Candidate Name'
+            placeholder="Enter Candidate Name"
+            value="Lionel Messi"
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>Candidate 2 Image URL</Form.Label>
           <Form.Control
             ref={candidateName2URL}
-            placeholder='enter Image URL'
+            placeholder="enter Image URL"
+            value="https://upload.wikimedia.org/wikipedia/commons/c/c1/Lionel_Messi_20180626.jpg"
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>Prompt</Form.Label>
-          <Form.Control ref={promptRef} placeholder='Add Prompt'></Form.Control>
+          <Form.Control ref={promptRef} placeholder="Add Prompt"></Form.Control>
         </Form.Group>
       </Form>
 
-      <Button onClick={sendToBloackChain} variant='primary'>Submit</Button>
+      <Button onClick={sendToBloackChain} variant="primary">
+        Submit
+      </Button>
     </Container>
   );
 };
